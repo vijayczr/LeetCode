e31135efc0853b1f2b20bf0fe5af3b999de6df07
vijay <vjvijay130@gmail.com>
2023-08-25 00:15:27 +0530

    two sum start

---
+++ Two Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> data = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Count(); i++)
+        {
+            if (data.ContainsKey(target - nums[i]))
+            {
+                return new int[] { data[target - nums[i]], i };
+            }
+            if (!data.ContainsKey(nums[i]))
+            {
+                data.Add(nums[i], i);
+            }
+        }
+        return new int[] { 0, 0 };
+    }
+}
