4ea96c4d8d385f14c143430df81babcc3e3ddbc1
unknown <vjvijay130@gmail.com>
2023-08-29 12:33:31 +0530

    palindrome number solution

---
+++ Palindrome Number/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Resullt
+{
+    public static void Main(string[] args)
+    {
+        int x  = int.Parse(Console.ReadLine());
+        bool m = IsPalindrome(x);
+
+        if(m == true)
+        {
+            Console.WriteLine("true");
+        }
+        else {  Console.WriteLine("false"); }
+
+
+
+        bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+            List<int> list = new List<int>();
+            while (x > 0)
+            {
+                list.Add(x % 10);
+                x = x / 10;
+            }
+            for (int i = 0; i < list.Count() / 2; i++)
+            {
+                if (list[i] != list[list.Count() - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+
+
+}
