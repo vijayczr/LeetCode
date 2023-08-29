1a74e59c95796ee49b66db7d569fac50475849aa
unknown <vjvijay130@gmail.com>
2023-08-29 15:38:47 +0530

    Longest Common Prefix

---
+++ Longest Common Prefeix/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// this wont be like copy paste answer . you have to modify it to worn in leetcode editor.
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the no of strings to enter : ");
+        int num = int.Parse(Console.ReadLine());
+        var ss = new List<string>();
+        for(int i = 0; i< num; i++)
+        {
+            var str = Console.ReadLine();
+            ss.Add(str);
+        }
+        string shortest = ss.OrderBy(s => s.Length).First();
+        int x = 0;
+        for (int i = 0; i < shortest.Length; i++)
+        {
+            if (ss.Select(s => s[i]).Distinct().Count() > 1)
+            {
+                Console.WriteLine(shortest[..i]);
+                x = 1;
+                break;
+            }
+        }
+        if(x==0)
+        {
+            Console.WriteLine(shortest);
+        }
+    }
+}
