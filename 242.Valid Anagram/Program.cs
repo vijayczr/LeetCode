92aaeb23185fdac74f418a02e4f61e8b2c81eda4
vijay <vjvijay130@gmail.com>
2023-09-03 02:16:40 +0530

    Valid Anagram 242

---
+++ 242.Valid Anagram/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+        var arr = new HashSet<char>(s);
+
+        foreach (var data in arr)
+        {
+            if (s.Count(e => e == data) != t.Count(w => w == data))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter strings!");
+        string s = Console.ReadLine();
+        string t = Console.ReadLine();
+        var g = new Solution();
+        bool n = g.IsAnagram(s, t);
+        Console.WriteLine(n);
+    }
+}
